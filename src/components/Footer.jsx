47177f531665c4ed8e5dbaa4e5a0a5c574b2b04f
Chimi <juan.ferrari304@gmail.com>
2023-03-30 01:54:39 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,10 @@
-import { useCart } from '../hooks/useCart'
 import './Footer.css'
 
 export function Footer () {
-  const { cart } = useCart()
-
   return (
     <footer className='footer'>
-      {/* {JSON.stringify(filters, null, 2)} */}
-      {JSON.stringify(cart, ['title', 'quantity'], 2)}
-      {/* <h4>Prueba técnica de React ⚛️ - <span>@midudev</span></h4>
-      <h5>Shopping Cart con useContext & useReducer</h5> */}
+      <h4>Prueba técnica de React ⚛️ - <span>@midudev</span></h4>
+      <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
   )
 }
